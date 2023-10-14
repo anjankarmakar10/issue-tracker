b2110badd47b7b7921255642895da33ae81f3d8a
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-10-14 14:28:54 +0600

    Style active link in the navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,5 +1,8 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { AiOutlineIssuesClose } from "react-icons/ai";
+import classNames from "classnames";
 const Navbar = () => {
   const links = [
     {
@@ -16,8 +19,10 @@ const Navbar = () => {
     },
   ];
 
+  const path = usePathname();
+
   return (
-    <header className="border-b h-[60px]">
+    <header className="border-b h-[60px] bg-white sticky z-50 top-0">
       <nav className="flex space-x-6 p-4  max-w-7xl mx-auto items-center">
         <Link
           className="text-rose-600 font-bold text-lg flex gap-1 items-center"
@@ -30,7 +35,12 @@ const Navbar = () => {
           {links.map((link) => (
             <Link
               key={link.label}
-              className="text-zinc-500 hover:text-zinc-800 transition-colors capitalize"
+              className={classNames({
+                "text-zinc-900 ": path === link.href,
+                "text-zinc-500 ": path !== link.href,
+                "hover:text-zinc-800 transition-colors capitalize font-medium":
+                  true,
+              })}
               href={link.href}
             >
               {link.label}
