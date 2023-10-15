06bfe799e0629ede58b086a1d8f9026640ee7266
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-10-15 10:15:53 +0600

    Customize Radix UI theme

--- app/About.tsx
+++ app/About.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 const About = () => {
   return (
-    <section className="bg-[#f8efd6]">
-      <div className="max-w-3xl mx-auto py-12 px-4 flex gap-5 flex-col text-lg">
+    <section className="bg-[#f8efd6] dark:text-slate-800">
+      <div className="max-w-3xl mx-auto py-12 px-4 flex gap-5 flex-col text-lg ">
         <p>
           Keeping customers happy is easier said than done, especially if you
           have a large customer base.&nbsp;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,17 @@
+import "@radix-ui/themes/styles.css";
+import "./theme-config.css";
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import Navbar from "./Navbar";
-import "@radix-ui/themes/styles.css";
-import { Theme } from "@radix-ui/themes";
+import { Theme, ThemePanel } from "@radix-ui/themes";
 
-const inter = Inter({ subsets: ["latin"] });
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,11 +24,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Theme>
+    <html lang="en" className={inter.variable}>
+      <body>
+        <Theme appearance="light" accentColor="violet">
           <Navbar />
           <main>{children}</main>
+          <ThemePanel />
         </Theme>
       </body>
     </html>
