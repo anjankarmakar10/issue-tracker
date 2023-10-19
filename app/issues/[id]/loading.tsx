02134495801041e106b3652051bb34597f75c679
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-10-19 12:49:56 +0600

    Refactor AssigneeSelect component

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -4,7 +4,7 @@ import { Box, Flex, Card } from "@radix-ui/themes";
 const LoadingIssueDetailPage = () => {
   return (
     <Box className="max-w-3xl mx-auto p-4 ">
-      <Skeleton />
+      <Skeleton width={"80%"} />
       <Flex className="space-x-3" my="2">
         <Skeleton width="5rem" />
         <Skeleton width="8rem" />
