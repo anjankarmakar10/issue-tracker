68727290b52aeb8f06c335b28b6d09e66b7111ec
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-10-19 20:36:19 +0600

    Implement pagination

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,19 @@
 import { Pagination } from "@/app/components";
 import { Container } from "@radix-ui/themes";
 
-export default function Dashboard() {
+export default function Dashboard({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
   return (
     <Container className="p-4">
       <div>Hello World</div>
-      <Pagination itemCount={100} pageSize={10} currentPage={10} />
+      <Pagination
+        itemCount={100}
+        pageSize={10}
+        currentPage={parseInt(searchParams.page)}
+      />
     </Container>
   );
 }
