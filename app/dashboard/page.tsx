7a1fc083fa30184893effc01b9bce6c42dc7dbe8
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-10-20 18:14:45 +0600

    Build the IssueSummary component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,21 @@
 import { Container } from "@radix-ui/themes";
-import LatestIssues from "./LatestIssues ";
+import prisma from "@/prisma/client";
+import IssueSummary from "./IssueSummary";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const open = await prisma.issue.count({
+    where: { status: "OPEN" },
+  });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({
+    where: { status: "CLOSED" },
+  });
+
   return (
     <Container className="p-4">
-      <LatestIssues />
+      <IssueSummary closed={closed} inProgress={inProgress} open={open} />
     </Container>
   );
 }
